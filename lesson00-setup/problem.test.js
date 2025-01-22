6fd46d1408c0eb8160beb4bd555502ea171a8402
BrandonBrown <brandonbrown@pursuit.org>
2025-01-22 11:16:56 -0500

    complete warmup

--- lesson00-setup/problem.test.js
+++ lesson00-setup/problem.test.js
@@ -1,7 +1,7 @@
-const sayHello = require("./problem");
+const sayHello = require('./problem');
 
-describe("sayHello", () => {
+describe('sayHello', () => {
   test("should return 'Hello, World!'", () => {
-    expect(sayHello()).toBe("Hello, World!");
+    expect(sayHello()).toBe('Hello, World!');
   });
 });
