2e7fe801f37e7a9e41da8a1058d664e27188d5c6
BrandonBrown <brandonbrown@pursuit.org>
2025-01-28 10:55:45 -0500

    save lesson02-strings progress

--- lesson02-strings/problem.js
+++ lesson02-strings/problem.js
@@ -6,6 +6,26 @@
  */
 function lengthOfLongestSubstring(s) {
   // Your code here
+  let longestSubstring = 0;
+  //split string into array
+  let stringToArr = s.split(' ');
+  // console.log('stringArr', stringToArr);
+
+  //count substrings in arr and return as arr
+  let stringIndexCount = stringToArr.map((el) => {
+    return el.length;
+  });
+  // console.log(stringIndexCount);
+
+  //return largest number in arr
+  stringIndexCount.forEach((el) => {
+    if (el > longestSubstring) {
+      longestSubstring = el;
+    }
+  });
+  return longestSubstring;
 }
+
+console.log(lengthOfLongestSubstring("Your're the most smartest person ever!"));
 
 module.exports = lengthOfLongestSubstring;
